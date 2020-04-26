bfa74ccad2746d375e272052abf67c7452fb4445
Dmitry Buyan <buyandl@mail.ru>
2020-04-26 17:20:00 +0400

    Подключение JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,6 @@
-let navigation = document.querySelector('.main-nav');
-let toggleButton = navigation.querySelector('.main-nav__toggle');
-let ratesModal = document.querySelector('.rates-modal');
+var navigation = document.querySelector('.main-nav');
+var toggleButton = navigation.querySelector('.main-nav__toggle');
+var ratesModal = document.querySelector('.rates-modal');
 
 navigation.classList.remove('main-nav--opened', 'main-nav--nojs');
 navigation.classList.add('main-nav--closed');
